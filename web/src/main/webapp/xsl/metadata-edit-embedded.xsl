aodn/core-geonetwork

commit 99879dee837ba8be76e4779f18370b874f3e5030

add element will now add language combo box if required

web/src/main/webapp/xsl/metadata-edit-embedded.xsl
@@ -13,7 +13,7 @@
 	<xsl:include href="metadata.xsl"/>
 
 	<xsl:template match="/">
-		<xsl:for-each select="/root/*[name(.)!='gui' and name(.)!='request']">
+		<xsl:for-each select="/root/*[name(.)!='gui' and name(.)!='request']//*[@geonet:addedObj = 'true']">
 			<xsl:apply-templates mode="elementEP" select=".">
 				<xsl:with-param name="edit" select="true()"/>
 				<xsl:with-param name="schema">

web/src/main/java/org/fao/geonet/kernel/EditLib.java
@@ -1416,4 +1416,19 @@ public class EditLib {
 		}
 	}
 
+	// -- The following methods are used by services that use metadata-edit-embedded so the
+	// -- classes know which element to transform
+	/**
+	 * Tag the element so the metaata-edit-embedded.xsl know which element is the element for display
+	 */
+    public static void tagForDisplay(Element elem) {
+        elem.setAttribute("addedObj","true", Edit.NAMESPACE);
+    }
+    /**
+     * Remove the tag element so the tag does not stay in the actual metadata.
+     */
+    public static void removeDisplayTag(Element elem) {
+        elem.removeAttribute("addedObj", Edit.NAMESPACE);
+    }
+
 }

web/src/main/java/org/fao/geonet/services/metadata/AddElement.java
@@ -29,10 +29,10 @@ import jeeves.server.ServiceConfig;
 import jeeves.server.UserSession;
 import jeeves.server.context.ServiceContext;
 import jeeves.utils.Util;
-import org.fao.geonet.GeonetContext;
+
 import org.fao.geonet.constants.Geonet;
 import org.fao.geonet.constants.Params;
-import org.fao.geonet.kernel.DataManager;
+import org.fao.geonet.kernel.EditLib;
 import org.jdom.Element;
 
 //=============================================================================
@@ -42,7 +42,7 @@ import org.jdom.Element;
 
 public class AddElement implements Service
 {
-	public void init(String appPath, ServiceConfig params) throws Exception {}
+    public void init(String appPath, ServiceConfig params) throws Exception {}
 
 	//--------------------------------------------------------------------------
 	//---
@@ -53,9 +53,6 @@ public class AddElement implements Service
 	public Element exec(Element params, ServiceContext context) throws Exception
 	{
 
-		GeonetContext gc = (GeonetContext) context.getHandlerContext(Geonet.CONTEXT_NAME);
-		DataManager   dataMan   = gc.getDataManager();
-
 		Dbms dbms = (Dbms) context.getResourceManager().open(Geonet.Res.MAIN_DB);
 		UserSession session = context.getUserSession();
 
@@ -64,10 +61,30 @@ public class AddElement implements Service
 		String name  = Util.getParam(params, Params.NAME);
 		String child = params.getChildText(Params.CHILD);
 
-		// -- build the element to be added and return it
+		// -- build the element to be added
+		// -- Here we do mark the element that is added
+		// -- then we traverse up the tree to the root 
+		// -- clone from the root and return the clone
+		// -- this is done so that the style sheets have 
+		// -- access to important information like the
+		// -- document language and other locales
+		// -- this is important for multilingual editing
+		// -- 
+		// -- Note that the metadata-embedded.xsl stylesheet
+		// -- only applies the templating to the added element, not to 
+		// -- the entire metadata so performance should not be a big issue
 		Element elResp = new AjaxEditUtils(context).addElementEmbedded(dbms, session, id, ref, name, child);
-		return elResp;
+        EditLib.tagForDisplay(elResp);
+        Element md = (Element) findRoot(elResp).clone();
+        EditLib.removeDisplayTag(elResp);
+
+        return md;
 	}
+
+    private Element findRoot( Element element ) {
+        if(element.isRootElement() || element.getParentElement() == null) return element;
+        return findRoot(element.getParentElement());
+    }
 }
 
 //=============================================================================

web/src/main/java/org/fao/geonet/services/metadata/DeleteElement.java
@@ -29,10 +29,10 @@ import jeeves.server.ServiceConfig;
 import jeeves.server.UserSession;
 import jeeves.server.context.ServiceContext;
 import jeeves.utils.Util;
-import org.fao.geonet.GeonetContext;
+
 import org.fao.geonet.constants.Geonet;
 import org.fao.geonet.constants.Params;
-import org.fao.geonet.kernel.DataManager;
+import org.fao.geonet.kernel.EditLib;
 import org.jdom.Element;
 
 //=============================================================================
@@ -58,10 +58,6 @@ public class DeleteElement implements Service
 
 	public Element exec(Element params, ServiceContext context) throws Exception
 	{
-
-		GeonetContext gc = (GeonetContext) context.getHandlerContext(Geonet.CONTEXT_NAME);
-		DataManager   dataMan   = gc.getDataManager();
-
 		UserSession session = context.getUserSession();
 		Dbms dbms = (Dbms) context.getResourceManager().open(Geonet.Res.MAIN_DB);
 
@@ -73,7 +69,13 @@ public class DeleteElement implements Service
 		for (int i = 0; i < ref.length; i++) {
 			child = new AjaxEditUtils(context).deleteElementEmbedded(dbms, session, id, ref[i], parentRef);
 		}
-		return child;
+
+		// -- The metadata-edit-embedded.xsl searches for a taged element to
+		// -- transform so tag the element for display
+		Element cloned = (Element) child.clone();
+		EditLib.tagForDisplay(cloned);
+
+		return cloned;
 	}
 }
 

commit b9e7ff60c7d69a4160e896ac06a817062c26da5f

Fix issue deleting metadata elements introduced in 99879dee837

web/src/main/webapp/xsl/metadata-edit-embedded.xsl
@@ -13,7 +13,7 @@
 	<xsl:include href="metadata.xsl"/>
 
 	<xsl:template match="/">
-		<xsl:for-each select="/root/*[name(.)!='gui' and name(.)!='request']//*[@geonet:addedObj = 'true']">
+		<xsl:for-each select="/root/*[name(.)!='gui' and name(.)!='request']/descendant-or-self::*[@geonet:addedObj = 'true']">
 			<xsl:apply-templates mode="elementEP" select=".">
 				<xsl:with-param name="edit" select="true()"/>
 				<xsl:with-param name="schema">