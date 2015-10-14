cf525f84d1ef40f5c70766f8a57827da93b29f8c
David Nahodil <dnahodil@utas.edu.au>
2015-10-14 12:24:06 +1100

    Remove tests which recheck behaviour that's checked elsewhere

--- web/src/test/java/org/fao/geonet/monitor/link/LinkInfoTest.java
+++ web/src/test/java/org/fao/geonet/monitor/link/LinkInfoTest.java
@@ -81,52 +81,19 @@ public class LinkInfoTest extends TestCase {
         assertEquals(FAILED, linkInfo.getStatus());
     }
 
-    public void testGetStatus() throws Exception {
+    public void testGetStatusNoChecks() throws Exception {
         LinkMonitorService.maxChecks = 3;
         LinkMonitorService.maxFailureRate = 0.5;
 
-        LinkInfo linkInfo = new LinkInfo(new LinkCheckerMock());
-        testResult = true;
+        attemptChecks(0, 0);
 
-        // No checks done - status should be UNKNOWN
-        assertEquals(LinkMonitorService.Status.UNKNOWN, linkInfo.getStatus());
-
-        // One check out of 3 done - status should be WORKING
-        linkInfo.check();
-        assertEquals(LinkMonitorService.Status.WORKING, linkInfo.getStatus());
-
-        // 3 checks done, should be WORKING
-        linkInfo.check();
-        linkInfo.check();
-        assertEquals(LinkMonitorService.Status.WORKING, linkInfo.getStatus());
-
-        // 1 check is now failing (out of 3), but we need only 50%, so still working
-        testResult = false;
-        linkInfo.check();
-        assertEquals(LinkMonitorService.Status.WORKING, linkInfo.getStatus());
-
-        // 2 checks out of 3 are failing, so it should be failing
-        linkInfo.check();
-        assertEquals(LinkMonitorService.Status.FAILED, linkInfo.getStatus());
+        assertEquals(UNKNOWN, linkInfo.getStatus());
     }
 
-    public void testGetStatusAllFailed() throws Exception {
-        LinkMonitorService.maxChecks = 10;
-        LinkMonitorService.maxFailureRate = 0.9;
-
-        LinkInfo linkInfo = new LinkInfo(new LinkCheckerMock());
-        testResult = false;
-
-        // Not enough checks done, but all have failed
-        linkInfo.check();
-        linkInfo.check();
-        assertEquals(LinkMonitorService.Status.FAILED, linkInfo.getStatus());
-    }
-
-    public void testCheck() throws Exception {
+    public void testGetCheckCount() throws Exception {
         // Verify we save only 3 checks back
         LinkMonitorService.maxChecks = 3;
-        LinkInfo linkInfo = new LinkInfo(new LinkCheckerMock());
+
         for (int i = 0; i < 10; i++) {
             linkInfo.check();
         }
