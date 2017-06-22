84ae6aaf7ff03ba313b6ab2c3081f1487d23b6b8
Alex McKeown <a.mckeown@utas.edu.au>
2017-06-22 11:00:27 +1000

    Make online resource monitor request 256 pixel tiles for wms check

--- web/src/main/java/org/fao/geonet/monitor/onlineresource/OnlineResourceCheckerWms.java
+++ web/src/main/java/org/fao/geonet/monitor/onlineresource/OnlineResourceCheckerWms.java
@@ -3,7 +3,7 @@ package org.fao.geonet.monitor.onlineresource;
 import org.jdom.Element;
 
 public class OnlineResourceCheckerWms extends OnlineResourceCheckerDefault {
-    private static String WMS_GET_MAP = "?service=WMS&request=GetMap&version=1.1.1&format=image/png&bbox=-180,-90,180,90&srs=EPSG:4326&width=1&height=1&STYLES=&layers=";
+    private static String WMS_GET_MAP = "?service=WMS&request=GetMap&version=1.1.1&format=image/png&bbox=-180,-90,180,90&srs=EPSG:4326&width=256&height=256&STYLES=&layers=";
 
     @Override
     public void setOnlineResource(String uuuid, final Element onlineResource) {
