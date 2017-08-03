71bb08c7d7174cc7184d7f15e773be7f6ecb0d17
pmbohm <pmbohm@utas.edu.au>
2017-08-03 10:59:54 +1000

    Fix typo in OnlineResourceCheckerWms

--- web/src/main/java/org/fao/geonet/monitor/onlineresource/OnlineResourceCheckerWms.java
+++ web/src/main/java/org/fao/geonet/monitor/onlineresource/OnlineResourceCheckerWms.java
@@ -6,7 +6,7 @@ public class OnlineResourceCheckerWms extends OnlineResourceCheckerDefault {
     private static String WMS_GET_MAP = "?service=WMS&request=GetMap&version=1.1.1&format=image/png&bbox=-180,-90,180,90&srs=EPSG:4326&width=256&height=256&STYLES=&layers=";
 
     @Override
-    public void setOnlineResource(String uuuid, final Element onlineResource) {
+    public void setOnlineResource(String uuid, final Element onlineResource) {
         super.setOnlineResource(uuid, onlineResource);
         String name = OnlineResourceCheckerUtils.parseOnlineResource(onlineResource, OnlineResourceCheckerUtils.NAME_XPATH);
         url += WMS_GET_MAP + name;
